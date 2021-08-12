6720eeb1798fb6c0bd4a5cd74c8b1f8210882245
Matt Maher <matt@maher-family.net>
2021-08-12 08:25:23 -0700

    GRADIENT DESCENT with array of arrays

--- linear-regression/index.js
+++ linear-regression/index.js
@@ -9,26 +9,28 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   {
     shuffle: true,
     splitTest: 50,
-    dataColumns: ['horsepower', 'weight', 'displacement'],
+    // dataColumns: ['horsepower', 'weight', 'displacement'],
+    dataColumns: ['horsepower'],
     labelColumns: ['mpg'],
   }
 );
-
+console.log(`[M@][index] starting`); // M@: logging
 const regression = new LinearRegression(features, labels, {
-  learningRate: 0.1,
-  iterations: 3,
-  batchSize: 10,
+  learningRate: 0.001,
+  iterations: 10,
 });
 
 regression.train();
-const r2 = regression.test(testFeatures, testLabels);
+console.log(`[M@][index] m, b `, regression.m, regression.b); // M@: logging
 
-plot({
-  x: regression.mseHistory.reverse(),
-  xLabel: 'Iteration #',
-  yLabel: 'Mean Squared Error',
-});
+// const r2 = regression.test(testFeatures, testLabels);
 
-console.log('R2 is', r2);
+// plot({
+//   x: regression.mseHistory.reverse(),
+//   xLabel: 'Iteration #',
+//   yLabel: 'Mean Squared Error',
+// });
 
-regression.predict([[120, 2, 380]]).print();
+// console.log('R2 is', r2);
+
+// regression.predict([[120, 2, 380]]).print();
