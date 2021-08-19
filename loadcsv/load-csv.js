786e204570f9bde261475b70cbdb028d2ac57189
Matt Maher <matt@maher-family.net>
2021-08-18 17:04:00 -0700

    done

--- loadcsv/load-csv.js
+++ loadcsv/load-csv.js
@@ -2,7 +2,23 @@ const fs = require('fs');
 const _ = require('lodash');
 const shuffleSeed = require('shuffle-seed');
 
-function loadCSV(filename, options) {
+function extractColumns(data, columnNames) {
+  const headers = _.first(data);
+  const indexes = _.map(columnNames, column => headers.indexOf(column));
+  const extracted = _.map(data, row => _.pullAt(row, indexes));
+
+  return extracted;
+}
+function loadCSV(
+  filename,
+  {
+    converters = {},
+    dataColumns = [],
+    labelColumns = [],
+    shuffle = true,
+    splitTest = false,
+  }
+) {
   let data = fs.readFileSync(filename, { encoding: 'utf-8' });
   data = data.split('\n').map(row => row.split(','));
   data = data.map(row => _.dropRightWhile(row, val => val === ''));
@@ -13,13 +29,48 @@ function loadCSV(filename, options) {
     if (index === 0) return row;
     // get values
     return row.map((element, index) => {
+      if (converters[headers[index]]) {
+        return converters[headers[index]](element);
+      }
       const result = parseFloat(element);
       return _.isNaN(result) ? element : result;
     });
   });
 
-  headers;
-  data;
+  let labels = extractColumns(data, labelColumns);
+  data = extractColumns(data, dataColumns);
+
+  labels.shift();
+  data.shift();
+
+  if (shuffle) {
+    data = shuffleSeed.shuffle(data, 'phrase1');
+    labels = shuffleSeed.shuffle(labels, 'phrase1');
+  }
+
+  if (splitTest) {
+    const trainSize = _.isNumber(splitTest)
+      ? splitTest
+      : Math.floor(data.length / 2);
+
+    return {
+      features: data.slice(0, trainSize),
+      labels: labels.slice(0, trainSize),
+      testFeatures: data.slice(trainSize),
+      testLabels: labels.slice(trainSize),
+    };
+  } else {
+    return { features: data, labels };
+  }
 }
 
-loadCSV('data.csv');
+const results = loadCSV('data.csv', {
+  dataColumns: ['height', 'value'],
+  labelColumns: ['passed'],
+  shuffle: true,
+  splitTest: 1,
+  converters: {
+    passed: val => val === 'TRUE',
+  },
+});
+results;
